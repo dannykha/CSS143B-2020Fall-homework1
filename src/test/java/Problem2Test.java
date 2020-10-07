b640132b54def89c7b9b8b52501bf7cb0a28c91e
danny <dannykha@uw.edu>
2020-10-06 21:57:05 -0700

    more work

--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import java.util.Arrays;
 
 public class Problem2Test {
 
@@ -12,5 +12,18 @@ public class Problem2Test {
     public void testBubbleSort() {
         // assertTrue(false);
         // add your tests here
+        int[] data = {5, 4 , 3, 2, 1};
+        int[] expected = {1, 2, 3, 4, 5};
+        Problem2.bubbleSort(data);
+
+        assertTrue(Arrays.equals(expected, data));
+
+
     }
+
 }
+
+
+
+
+
